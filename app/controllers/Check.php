39409271d9c26987005c4a0558ef922d3cca0854
Ridwan Salamony <ridwansalamony@gmail.com>
2023-09-30 01:04:31 +0700

    add fiture check mstran

--- app/controllers/Check.php
+++ app/controllers/Check.php
@@ -21,6 +21,103 @@ class Check extends Controller
         $this->view('layouts/footer');
     }
 
+    public function mstran()
+    {
+        $data['title'] = 'Check Data';
+        $data['nav'] = 'Mstran';
+        $data['user'] = $_SESSION['nama'];
+        $this->view('layouts/header', $data);
+        $this->view('layouts/navbar', $data);
+        $this->view('check/mstran');
+        $this->view('layouts/footer');
+    }
+
+    public function mstranup()
+    {
+        if (isset($_POST['submit'])) {
+            $plu = $_POST['plu'];
+            $npb = $_POST['npb'];
+            $tanggal_proses = $_POST['tanggal_proses'];
+
+            $toko = $this->model('StoreModel')->getStoreByCode();
+
+            if (!$toko) {
+                Flasher::setFlash("<span class='font-bold'>KODE TOKO TIDAK ADA!</span>", "Silahkan tambah di menu daftar toko", 'red');
+                header('Location: ' . BASEURL . 'check/mstran');
+                exit;
+            } else {
+                $user = DB_USER_TOKO;
+                $name = DB_NAME_TOKO;
+                if ($_POST['pass'] === 'old') {
+                    $pass = DB_PASS_TOKO_OLD;
+                } else {
+                    $pass = DB_PASS_TOKO_NEW;
+                }
+
+                $ip = $toko['induk'];
+                $kode = $toko['toko'];
+
+                // Eksekusi
+                $dsn = "mysql:host=$ip;dbname=$name";
+                $option = [
+                    PDO::ATTR_PERSISTENT => true,
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                ];
+
+                try {
+                    $conn = new PDO($dsn, $user, $pass, $option);
+
+                    $mstran = "SELECT * FROM mstran WHERE prdcd LIKE '%$plu%' AND invno LIKE '%$npb%' AND bukti_tgl LIKE '%$tanggal_proses%' ORDER BY bukti_tgl DESC";
+
+                    $stmt = $conn->prepare($mstran);
+
+                    $stmt->execute();
+
+                    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                    if (!$result) {
+                        Flasher::setFlash("<span class='font-bold'>DATA TIDAK ADA!</span>", "Masukkan DATA yang valid", 'red');
+                        header('Location: ' . BASEURL . 'check/mstran');
+                        exit;
+                    } else {
+                        foreach ($result as $item) {
+                            $npb = $item['INVNO'];
+                            $prdcd = $item['PRDCD'];
+                            $rtype = $item['RTYPE'];
+                            $keter = $item['KETER'];
+                            $bukti = $item['BUKTI_NO'];
+                            $tanggal = $item['BUKTI_TGL'];
+
+                            $result[] = array(
+                                'npb' => $npb,
+                                'prdcd' => $prdcd,
+                                'rtype' => $rtype,
+                                'keter' => $keter,
+                                'bukti' => $bukti,
+                                'tanggal' => $tanggal,
+                            );
+                            $conn = null;
+                        }
+
+                        $data['title'] = 'Check Data';
+                        $data['nav'] = 'Mstran';
+                        $data['user'] = $_SESSION['nama'];
+                        $data['result'] = $result;
+                        $this->view('layouts/header', $data);
+                        $this->view('layouts/navbar', $data);
+                        $this->view('check/mstran', $data);
+                        $this->view('layouts/footer');
+                    }
+                } catch (PDOException $e) {
+                    Flasher::setFlash("<span class='font-bold'>PROSES GAGAL</span>", "Koneksi <span class='font-bold text-info'>$kode</span> down / Pass SQL Salah!", 'red');
+                    header('Location: ' . BASEURL . 'check/mstran');
+                }
+            }
+        } else {
+            header('Location: ' . BASEURL . 'check/mstran');
+        }
+    }
+
     public function stmast()
     {
         $data['title'] = 'Check Data';
@@ -102,6 +199,7 @@ class Check extends Controller
                         }
 
                         $data['title'] = 'Check Data';
+                        $data['nav'] = 'Stmast';
                         $data['user'] = $_SESSION['nama'];
                         $data['result'] = $result;
                         $this->view('layouts/header', $data);
