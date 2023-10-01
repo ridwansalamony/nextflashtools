87e90a056d44a1b6e858fc18020adfa7704f382c
Ridwan Salamony <ridwansalamony@gmail.com>
2023-10-01 13:12:07 +0700

    add fiture reopen check

--- app/controllers/Pb.php
+++ app/controllers/Pb.php
@@ -86,4 +86,70 @@ class Pb extends Controller
             exit;
         }
     }
+
+    public function reopenpbx()
+    {
+        $data['title'] = 'Beranda';
+        $data['nav'] = 'Reopen Cek';
+        $data['user'] = $_SESSION['nama'];
+        $this->view('layouts/header', $data);
+        $this->view('layouts/navbar', $data);
+        $this->view('pb/reopenpbx');
+        $this->view('layouts/footer');
+    }
+
+    public function reopenpbxup()
+    {
+        if (isset($_POST['submit'])) {
+
+            $toko = $this->model('StoreModel')->getStoreByCode();
+
+            $picnot = $_POST['picnot'];
+            $docno = $_POST['npb'];
+
+            if (!$toko) {
+                Flasher::setFlash('<span class="font-bold">PROSES GAGAL!</span> Data toko <span class="text-info font-bold uppercase">' . $_POST['kode_toko'] . '</span> tidak ada!', 'Silahkan tambah di menu Daftar Toko', 'red');
+                header('Location: ' . BASEURL . 'pb/reopenpbx');
+                exit;
+            } else {
+                $user = DB_USER_TOKO;
+                $name = DB_NAME_TOKO;
+                if ($_POST['pass'] === 'old') {
+                    $pass = DB_PASS_TOKO_OLD;
+                } else {
+                    $pass = DB_PASS_TOKO_NEW;
+                }
+
+                $ip = $toko['induk'];
+                $kode = $toko['toko'];
+
+                // Eksekusi
+                $dsn = "mysql:host=$ip;dbname=$name";
+                $option = [
+                    PDO::ATTR_PERSISTENT => true,
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                ];
+
+                try {
+                    $conn = new PDO($dsn, $user, $pass, $option);
+
+                    $query = "UPDATE dcpbox_plu SET recid='$picnot' WHERE docno='$docno'";
+
+                    $stmt1 = $conn->prepare($query);
+
+                    $stmt1->execute();
+
+                    Flasher::setFlash("<span class='font-bold'>PROSES BERHASIL</span> <span class='font-bold text-info uppercase'>$kode</span>", "Silahkan buka ulang Program Cek Barang dan coba lagi", 'blue');
+                    header('Location: ' . BASEURL . 'pb/reopenpbx');
+                    $conn = null;
+                } catch (PDOException $e) {
+                    Flasher::setFlash("<span class='font-bold'>PROSES GAGAL</span>", "Koneksi <span class='font-bold text-info uppercase'>$kode</span> down / Pass SQL Salah!", 'red');
+                    header('Location: ' . BASEURL . 'pb/reopenpbx');
+                }
+            }
+        } else {
+            header('Location: ' . BASEURL . 'pb/reopenpbx');
+            exit;
+        }
+    }
 }
