feca81815fac83ad777991da3bc16376e9314bc3
Ridwan Salamony <ridwansalamony@gmail.com>
2023-10-04 14:37:08 +0700

    edit style view login

--- app/views/guest/login.php
+++ app/views/guest/login.php
@@ -32,7 +32,7 @@
                     </svg>
                     <span class="sr-only">Info</span>
                     <div>
-                        <span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+                        <span class="font-medium text-warning">Info alert!</span> Change a few things up and try submitting again.
                     </div>
                 </div>
                 <div class="hidden items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
